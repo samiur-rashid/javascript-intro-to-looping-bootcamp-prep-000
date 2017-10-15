c6b5377b898f2a3b0b374f48b871e7a9d2318327
Samiur Rashid <samrashid@gmail.com>
2017-10-15 04:38:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
   var array = []
 
 
-  for (let i = 0; i <= 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array[i] = "I am 1 strange loop."
     }
