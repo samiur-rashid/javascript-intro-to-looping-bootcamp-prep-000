29fcdc6e66ada56f606a127d1851bdfe691d034a
Samiur Rashid <samrashid@gmail.com>
2017-10-15 04:26:36 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+var array = []
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i===1) {
+      array[i] = "I am 1 strange loop."
+    }
+    else {
+      array[i] = "I am ${i} strange loops."
+    }
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+
+  do {
+
+    function maybeTrue() {
+      array.splice(array.length)
+    }
+
+  } while(array.length > 0 || maybeTrue())
+
+  return array
+}
