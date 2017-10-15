789c6081638786e3f945a7f0efb53e21e642bc2e
Samiur Rashid <samrashid@gmail.com>
2017-10-15 04:45:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ function forLoop(array) {
       array[i] = "I am 1 strange loop."
     }
     else {
-      array[i] = "I am ${i} strange loops."
+      array[i] = "I am " + i + " strange loops."
     }
   }
   return array
